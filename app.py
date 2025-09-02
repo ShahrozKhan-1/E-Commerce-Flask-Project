0dca6cae0ef0729da82f8d93fc3b979c0e583328
Shahroz Khan <shahrozkha83@gmail.com>
2025-09-02 18:13:41 +0500

    deployment

--- app.py
+++ app.py
@@ -1,49 +1,48 @@
-from flask import Flask, current_app
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from config import Config
 from flask_migrate import Migrate
-
-
+from config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
-def create_app(config_class = Config):
+def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    
-    from models import Category
+
+    # Import models after db init
+    from models import Category, User
+
+    # Context processor (safe query)
     @app.context_processor
     def inject_categories():
-        categories = Category.query.all()
+        try:
+            categories = Category.query.all()
+        except Exception:
+            categories = []
         return dict(categories=categories)
-    
+
     db.init_app(app)
-    
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
-    
-    migrate = Migrate(app, db)
+    Migrate(app, db)
 
+    # Blueprints
     from apps.auth.auth import auth
     from apps.products.products import product
-    from models import User
-    
     app.register_blueprint(auth)
     app.register_blueprint(product)
 
-
+    # Flask-Login user loader
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-    
-    with app.app_context():
-        print(current_app.name)
-        db.create_all()
-    
-        
+
     return app
 
-if __name__ == '__main__':
-    create_app().run()
+# For deployment (Gunicorn looks for 'app')
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
